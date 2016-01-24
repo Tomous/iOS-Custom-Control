cbd57da599d6e099145e92bcfe2684327e0c99f2
SSBun <1150183856@qq.com>
2016-01-24 17:54:31 +0800

    滚动横幅（3）

---
+++ 滚动横幅（3）/BZScrollBar.h
@@ -0,0 +1,15 @@
+//
+//  BZScrollBar.h
+//  BZScrollBar
+//
+//  Created by 蔡士林 on 16/1/22.
+//  Copyright © 2016年 BZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BZScrollBar : UIView
+
+@property (strong, nonatomic) NSArray *datas;
+
+@end
